7197a5c749ef7bb31de7eac05bd601b1e3a70ba9
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-02-22 17:25:13 -0800

    refactor(integratinsettings): changed url link

--- src/integrationSpecFile.ts
+++ src/integrationSpecFile.ts
@@ -1,4 +1,4 @@
-const url = 'https://f7xjdx7g-8000.eun1.devtunnels.ms/recommendation'
+const url = 'https://smart-gift-bot-git-main-reaganz-wats-projects.vercel.app/recommendation'
 
 export const smartGiftBotConfig = {
   data: {
